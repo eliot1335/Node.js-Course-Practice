72284d9e97b13a2d8e566925938bcc9e1f579ab4
Eliot Chern <eliotchern@gmail.com>
2021-05-19 12:47:16 -0700

    testing loading my own file

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-// fs.writeFileSync('notes.txt', 'This file was created by node.js!')
+// // fs.writeFileSync('notes.txt', 'This file was created by node.js!')
 
-fs.appendFileSync('notes.txt', "let's see how this goes!")
+// fs.appendFileSync('notes.txt', "let's see how this goes!")
+
+require('./utils.js')
+
+console.log('hmm')
