dd2a4849f6e7fd69ecd09406f3dfcd38a8641def
Eliot Chern <eliotchern@gmail.com>
2021-05-19 12:43:39 -0700

    testing fs module append method

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'This file was created by node.js!')
+// fs.writeFileSync('notes.txt', 'This file was created by node.js!')
+
+fs.appendFileSync('notes.txt', "let's see how this goes!")
