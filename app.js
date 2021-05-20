ce8c79a416d6e4d55ea15cfd6f9fba50238bc730
Eliot Chern <eliotchern@gmail.com>
2021-05-20 02:52:18 -0700

    test command line arguments

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const chalk = require("chalk")
+const yargs = require("yargs")
 const validator = require("validator")
 const getNotes = require('./notes.js')
 
+/*
 const command = process.argv[2]
 
 console.log(process.argv)
+
 
 if (command === "add") {
     console.log("Adding Note!")
@@ -13,3 +16,47 @@ if (command === "add") {
 else if (command === "remove") {
     console.log("Removing Note!")
 }
+*/
+
+
+// Customize yargs version
+yargs.version("1.1.0")
+
+// Create add command
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    handler: function() {
+        console.log("Adding a new note!")
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: function() {
+        console.log("Removing the note!")
+    }
+})
+
+// Create list command
+yargs.command({
+    command: "list",
+    describe: "List your notes",
+    handler: function() {
+        console.log("Listing out all notes!")
+    }
+})
+
+// Create read command
+yargs.command({
+    command: "read",
+    describe: "Read a note",
+    handler: function() {
+        console.log("Reading a note!")
+    }
+})
+
+
+console.log(yargs.argv)
