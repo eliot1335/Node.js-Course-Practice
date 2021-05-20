d8cb0bf702a6d0bc2d22d65d746aabf3200b5e39
Eliot Chern <eliotchern@gmail.com>
2021-05-20 02:38:42 -0700

    use process.argv to take user input

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-// const fs = require('fs')
-
-// // fs.writeFileSync('notes.txt', 'This file was created by node.js!')
-
-// fs.appendFileSync('notes.txt', "let's see how this goes!")
-
 const chalk = require("chalk")
 const validator = require("validator")
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
+const command = process.argv[2]
 
-console.log(msg)
+console.log(process.argv)
 
-console.log(validator.isEmail("eliottesttest@gmail.com"))
-console.log(validator.isURL("https://www.npmjs.com"))
-console.log(chalk.green.bold("show me the green!"))
+if (command === "add") {
+    console.log("Adding Note!")
+}
 
-console.log(process.argv[2])
+else if (command === "remove") {
+    console.log("Removing Note!")
+}
