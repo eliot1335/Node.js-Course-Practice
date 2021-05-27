6254f1c7fb1dd06cd92e18bcd578359fbd050ec4
Eliot Chern <eliotchern@gmail.com>
2021-05-27 01:22:21 -0700

    chalk user feedback feature

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require("fs")
+const chalk = require("chalk")
 
 const getNotes = function(){
     return "Your notes..."
@@ -17,12 +18,12 @@ const addNote = function(title, body){
         })
 
         saveNotes(notes)
-        console.log("New note added!")
+        console.log(chalk.green.inverse("New note added!"))
 
     }
 
     else {
-        console.log("Note title taken")
+        console.log(chalk.red.inverse("Note title taken"))
     }
 }
 
@@ -31,6 +32,14 @@ const removeNote = function(title){
     const notesToKeep =  notes.filter(function(note){
         return note.title !== title
     })
+
+    if (notes.length > notesToKeep.length){
+        console.log(chalk.green.inverse("Note removed!"))
+    }
+
+    else {
+        console.log(chalk.red.inverse("No note found!"))
+    }
 
     saveNotes(notesToKeep)
 }
